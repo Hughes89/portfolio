cbcc61d551c794aaf617dcfd59281ddb349fd5f3
Hughes89 <mik3h89@gmail.com>
2017-02-10 12:33:02 -0500

    Feat: integrate skills into the about controller

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -5,11 +5,40 @@
     .module('app')
     .controller('AboutController', AboutController);
 
-    AboutController.$inject = [];
+    AboutController.$inject = ['$timeout'];
 
-    function AboutController () {
+    function AboutController ($timeout) {
       var vm = this;
-      vm.welcome = "About!!!";
+      vm.skills = {};
+      vm.skills._one = window.skills.one;
+      vm.skills._two = window.skills.two;
+      $timeout(() => progressBars(vm.skills));
+
+
+      function progressBars (skills) {
+        skills._one.forEach(skill => {
+          var progress = new ProgressBar.Line('#' + skill.id, {
+            strokeWidth: 4,
+            easing: 'easeInOut',
+            duration: 2500,
+            color: skill.color,
+            trailColor: '#8e8e8e',
+            trailWidth: 1,
+          });
+          progress.animate(skill.percentage * 0.01);
+        });
+        skills._two.forEach(skill => {
+          var progress = new ProgressBar.Line('#' + skill.id, {
+            strokeWidth: 4,
+            easing: 'easeInOut',
+            duration: 2500,
+            color: skill.color,
+            trailColor: '#8e8e8e',
+            trailWidth: 1,
+          });
+          progress.animate(skill.percentage * 0.01);
+        });
+      }
     }
 
 })();
---
+++ client/app/about/skills.js
@@ -0,0 +1,102 @@
+(function () {
+  'use strict';
+
+  window.skills = {};
+  window.skills.one = [{
+      id: "s1",
+      name: "JavaScript", 
+      percentage: 95,
+      color: "#ffd248"
+    }, 
+    {
+      id: "s2",
+      name: "TypeScript", 
+      percentage: 75,
+      color: "#F79622"
+    }, 
+    {
+      id: "s3",
+      name: "HTML/CSS", 
+      percentage: 95,
+      color: "#48A9A6"
+    }, 
+    {
+      id: "s4",
+      name: "jQeury", 
+      percentage: 90,
+      color: "#D46271"
+    }, 
+    {
+      id: "s5",
+      name: "Angular", 
+      percentage: 90,
+      color: "#6AB083"
+    }, 
+    {
+      id: "s6",
+      name: "Angular 2", 
+      percentage: 85,
+      color: "#A4DD80"
+    }, 
+    {
+      id: "s7",
+      name: "React", 
+      percentage: 90,
+      color: "#F6B277"
+    },{
+      id: "s8",
+      name: "React Native", 
+      percentage: 80,
+      color: "#aec5e6"
+    }, ];
+
+  window.skills.two = [{
+      id: "s9",
+      name: "Backbone", 
+      percentage: 65,
+      color: "#6EA4BF"
+    }, 
+    {
+      id: "s10",
+      name: "Node/Express", 
+      percentage: 95,
+      color: "#EF767A"
+    }, 
+    {
+      id: "s11",
+      name: "MongoDB", 
+      percentage: 95,
+      color: "#FFBA49"
+    }, 
+    {
+      id: "s12",
+      name: "MySQL", 
+      percentage: 90,
+      color: "#A1CF6B"
+    }, 
+    {
+      id: "s13",
+      name: "Git", 
+      percentage: 95,
+      color: "#FFE066"
+    }, 
+    {
+      id: "s14",
+      name: "Gulp", 
+      percentage: 85,
+      color: "#63CCCA"
+    }, 
+    {
+      id: "s15",
+      name: "Webpack",
+      percentage: 85,
+      color: "#4a9ccc"
+    },
+    {
+      id: "s16",
+      name: "Mocha/Chai", 
+      percentage: 65,
+      color: "#397367"
+    }];
+
+})();
